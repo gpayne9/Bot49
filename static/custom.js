6969bed2c9c4a49f16c74d3fe5ae82f693bb074e
Guy <gpayne9@uncc.edu>
2018-12-06 13:57:47 -0500

    final commit

--- static/custom.js
+++ static/custom.js
@@ -17,6 +17,24 @@ function submit_message(message) {
 
 			$.post("/bot_49", {message: data.message, intent: data.intent}, handle_pictures);
 		}
+		else if(data.intent == "resource"){
+			$('.chat-container').append(`<div class="chat-message col-md-5 offset-md-7 bot-message"><a href ="https://${data.message}" style="color:#7CFC00	"> ${data.message} </a></div>`)
+
+		}
+		else if(data.intent == "food"){
+			// $('.chat-container').append(`<div class="chat-message col-md-5 offset-md-7 bot-message">
+			// <p>Food options on campus by building<br> You can get directions to the restaurants. examp "How do I get to Wendy's?"
+			// <p><b>Student Union</b>  <br>-Bojangles<br>-Crown<br>-Bistro 49 <br>-Outtakes<br>-Sambazon<br>-Starbucks<br>-Wendy's<br>-Einstein Bros. Bagels<br></p>
+			// <p><b>South Village Crossing</b>  <br>-Sovi<br>-Sovi2Go<br>-The Den<br>-Outtakes<br></p>
+			// <p><b>Prospector</b> <br>-Smoked<br>-Chick-fil-A<br>-Mondo Subs<br>-Salsarita's<br>-Mamma Leone's<br>Feisty's<br></p>
+			// <p><b>Cone Universtiy Center</b>  <br>-Subway<br>-Panda Express<br>-Bubble Tea<br> </p>
+			// <p><b>Atkins Library</b>: Library Cafe </p>
+			// <p><b>Student Activity Center</b>: Papa John's Pizza </p>
+			// <p><b>Fretwell</b>: Fretwell Cafe </p>
+			$('.chat-container').append(`<div class="chat-message col-md-5"> <img src="/static/images/Dining_Options.png" width="600" height="475"></img> 
+			<p><b> Dining Hours and map: <b> <a href ="https://aux.uncc.edu/dining" style="color:#7CFC00	">aux.uncc.edu/dining</a>
+			</div>`)
+		}
 		else {
 			$('.chat-container').append(`<div class="chat-message col-md-5 offset-md-7 bot-message">${data.message}</div>`)
 		}
@@ -31,9 +49,13 @@ function submit_message(message) {
 
 	function handle_pictures(data){
 		$('.chat-container').append(`<div class="chat-message col-md-5"> <img id="theImg" src=${data} style="max-width:auto; height:100%;"> </img> </div>`)
-
+	
 		// $('#theDiv').prepend('<img id="theImg" src="theImg.png" />')
 	}
+	function handle_resource(data){
+		$('.chat-container').append(`<div class="chat-message col-md-5"> <img id="theImg" src=${data} style="max-width:auto; height:100%;"> </img> </div>`)
+	}
+
 }
 
 
